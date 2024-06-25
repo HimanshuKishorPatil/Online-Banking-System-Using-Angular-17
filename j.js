c59ddb0c56476e1dad2dbae30aa2889fcecc95ae
HimanshuKishorPatil <134077678+HimanshuKishorPatil@users.noreply.github.com>
2024-06-25 16:28:32 +0530

    Delete j.js

--- j.js
+++
@@ -1,7 +0,0 @@
-// const arr=[10,20,30]
-from ([10,20,30]).subscribe( 
-    next => console.log('next:', next), 
-    err => console.log('error:', err), 
-    () => console.log('the end'), 
-    );
-    
